fdf219ca53d50a997811f768ed5d558757d107b0
theri <therigasmazurets@gmail.com>
2023-12-23 15:25:37 +0200

    up

--- app/src/main/java/com/example/firebase/fragments/MainFragment.kt
+++ app/src/main/java/com/example/firebase/fragments/MainFragment.kt
@@ -1,19 +1,42 @@
 package com.example.firebase.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.firebase.R
+import com.example.firebase.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() = with(binding) {
+        btnRegister.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.placeHolder, RegisterFragment.newInstance())
+                .addToBackStack(null)
+                .commit()
+        }
+        btnLogin.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.placeHolder, LoginFragment.newInstance())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/firebase/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/firebase/fragments/RegisterFragment.kt
@@ -5,56 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.firebase.R
+import com.example.firebase.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+    private fun init() = with(binding){
+        firebaseAuth = FirebaseAuth.getInstance()
+        btnRegister.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if(email.isEmpty())
+                Toast.makeText(context, "Email is empty", Toast.LENGTH_LONG).show()
+            else if(password.isEmpty())
+                Toast.makeText(context, "Password is empty", Toast.LENGTH_LONG).show()
+            else if(password.length < 6)
+                Toast.makeText(context, "Password is too small", Toast.LENGTH_LONG).show()
+            else {
+                registerUser(email, password)
+            }
+        }
+    }
+
+    private fun registerUser(email: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = RegisterFragment()
     }
 }
